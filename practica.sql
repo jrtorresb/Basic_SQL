dc4118fcb2607d533cb465221096d3915480fee4
jrtorresb <37489423+jrtorresb@users.noreply.github.com>
2020-01-07 12:15:15 -0600

    Add files via upload

---
+++ practica.sql
@@ -0,0 +1,87 @@
+-- 0. Drop table INSTRUCTOR in case it already exists
+drop table INSTRUCTOR
+;
+--1. Create table INSTRUCTOR
+CREATE TABLE INSTRUCTOR
+  (ins_id INTEGER PRIMARY KEY NOT NULL, 
+   lastname VARCHAR(15) NOT NULL, 
+   firstname VARCHAR(15) NOT NULL, 
+   city VARCHAR(15), 
+   country CHAR(2)
+  )
+;
+--2A. Insert single row for Rav Ahuja
+INSERT INTO INSTRUCTOR
+  (ins_id, lastname, firstname, city, country)
+  VALUES 
+  (1, 'Ahuja', 'Rav', 'Toronto', 'CA')
+;
+--2B. Insert the two rows for Raul and Hima
+INSERT INTO INSTRUCTOR
+  VALUES
+  (2, 'Chong', 'Raul', 'Toronto', 'CA'),
+  (3, 'Vasudevan', 'Hima', 'Chicago', 'US')
+;
+--3. Select all rows in the table
+SELECT * FROM INSTRUCTOR
+;
+--3b. Select firstname, lastname and country where city is Toronto
+SELECT firstname, lastname, country from INSTRUCTOR where city='Toronto'
+;
+--4. Change the city for Rav to Markham
+UPDATE INSTRUCTOR SET city='Markham' where ins_id=1
+;
+--5. Delete the row for Raul Chong
+DELETE FROM INSTRUCTOR where ins_id=2
+;
+--5b. Retrieve all rows from the table
+SELECT * FROM INSTRUCTOR 
+
+
+
+
+
+------------------------------------------
+--DDL statement for table 'HR' database--
+--------------------------------------------
+
+CREATE TABLE EMPLOYEES (
+                            EMP_ID CHAR(9) NOT NULL, 
+                            F_NAME VARCHAR(15) NOT NULL,
+                            L_NAME VARCHAR(15) NOT NULL,
+                            SSN CHAR(9),
+                            B_DATE DATE,
+                            SEX CHAR,
+                            ADDRESS VARCHAR(30),
+                            JOB_ID CHAR(9),
+                            SALARY DECIMAL(10,2),
+                            MANAGER_ID CHAR(9),
+                            DEP_ID CHAR(9) NOT NULL,
+                            PRIMARY KEY (EMP_ID));
+                            
+  CREATE TABLE JOB_HISTORY (
+                            EMPL_ID CHAR(9) NOT NULL, 
+                            START_DATE DATE,
+                            JOBS_ID CHAR(9) NOT NULL,
+                            DEPT_ID CHAR(9),
+                            PRIMARY KEY (EMPL_ID,JOBS_ID));
+ 
+ CREATE TABLE JOBS (
+                            JOB_IDENT CHAR(9) NOT NULL, 
+                            JOB_TITLE VARCHAR(15) ,
+                            MIN_SALARY DECIMAL(10,2),
+                            MAX_SALARY DECIMAL(10,2),
+                            PRIMARY KEY (JOB_IDENT));
+
+CREATE TABLE DEPARTMENTS (
+                            DEPT_ID_DEP CHAR(9) NOT NULL, 
+                            DEP_NAME VARCHAR(15) ,
+                            MANAGER_ID CHAR(9),
+                            LOC_ID CHAR(9),
+                            PRIMARY KEY (DEPT_ID_DEP));
+
+CREATE TABLE LOCATIONS (
+                            LOCT_ID CHAR(9) NOT NULL,
+                            DEP_ID_LOC CHAR(9) NOT NULL,
+                            PRIMARY KEY (LOCT_ID,DEP_ID_LOC));
+                            
